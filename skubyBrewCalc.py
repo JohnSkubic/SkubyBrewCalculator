28de730785287ffe0540b8f9b4cb159a25801703
John Skubic <jskubic@purdue.edu>
2017-12-29 16:39:25 -0600

    Added basic GUI setup and top level script

---
+++ skubyBrewCalc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+################################################################################
+#
+# Refer to "LICENSE" for licensing information
+#
+# Author: John Skubic
+# Email:  skubicjj@gmail.com
+# Date:   12/16/17 
+#
+# Description:  
+#
+################################################################################
+
+from SkubyBrewCalc.SkubyGUI.SkubyGUI import *
+import sys
+
+if __name__ == "__main__":
+  launchSkubyGUI(sys.argv)
